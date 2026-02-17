64e526cd3b09da640e2d6fba7493536148a5471f
Pravisht <raghavgoel1305@gmail.com>
2026-02-18 01:44:48 +0530

    Refactor import statement for PostConstruct to use jakarta.annotation instead of javax.annotation

--- src/main/java/com/example/ExamPractice/TesseractLibraryConfig.java
+++ src/main/java/com/example/ExamPractice/TesseractLibraryConfig.java
@@ -1,11 +1,11 @@
 package com.example.ExamPractice;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.io.File;
 
 /**
